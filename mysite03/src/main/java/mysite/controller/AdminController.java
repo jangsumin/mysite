b3a086aca259185e2ad7997932cdeacf18515f23
jangsumin <jsm0726@naver.com>
2025-01-03 17:45:26 +0900

    feat: 1월 3일 과제 업로드(인터셉터를 활용하여 SiteService에서 변경한 내용 반영)

--- mysite03/src/main/java/mysite/controller/AdminController.java
+++ mysite03/src/main/java/mysite/controller/AdminController.java
@@ -1,9 +1,15 @@
 package mysite.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import mysite.security.Auth;
+import mysite.service.FileUploadService;
+import mysite.service.SiteService;
+import mysite.vo.SiteVo;
 
 // 클래스 내 모든 메서드는 admin만 접근할 수 있는 메서드다.
 @Auth(role="ADMIN")
@@ -11,8 +17,18 @@ import mysite.security.Auth;
 @RequestMapping("/admin")
 public class AdminController {
 	
+	private SiteService siteService;
+	private FileUploadService fileUploadService;
+	
+	public AdminController(SiteService siteService, FileUploadService fileUploadService) {
+		this.siteService = siteService;
+		this.fileUploadService = fileUploadService;
+	}
+	
 	@RequestMapping({"", "/main"})
-	public String main() {
+	public String main(Model model) {
+		SiteVo siteVo = siteService.getSite();
+		model.addAttribute("siteVo", siteVo);
 		return "admin/main";
 	}
 	
@@ -29,5 +45,23 @@ public class AdminController {
 	@RequestMapping("/user")
 	public String user() {
 		return "admin/user";
+	}
+	
+	@RequestMapping("/main/update")
+	public String updateSite(@RequestParam("title") String title, @RequestParam("welcome") String welcome, @RequestParam("file") MultipartFile file, @RequestParam("description") String description) {
+		String profile = fileUploadService.restore(file);
+		
+		SiteVo siteVo = new SiteVo();
+		siteVo.setTitle(title);
+		siteVo.setWelcome(welcome);
+		siteVo.setProfile(profile);
+		siteVo.setDescription(description);
+		
+		System.out.println(title);
+		System.out.println(welcome);
+		System.out.println(profile);
+		System.out.println(description);
+		siteService.updateSite(siteVo);
+		return "redirect:/";
 	}
 }
